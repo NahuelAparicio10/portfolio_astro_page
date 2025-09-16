f23742da1946e770727abacef3b52be6c404950b
Nahuel Aparicio <nahuel.ap.code@gmail.com>
2025-09-16 16:30:17 +0200

    bug_deploying

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import tailwindcss from "@tailwindcss/vite";
-// @ts-check
 
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
     site: 'https://NahuelAparicio10.github.io',
   integrations: [mdx(), sitemap()],
